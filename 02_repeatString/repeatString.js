1183bf529c99865c7f427efa13990a0eb9465baa
Justin Tan <tanjustin217@gmail.com>
2024-08-03 09:00:21 -0600

    Complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, count) {
+  if (count < 0) {
+    return "ERROR";
+  } else {
+    let bigString = "";
+    for (i = 1; i <= count; i++) {
+      bigString += string;
+    }
+    return bigString;
+  }
 };
 
 // Do not edit below this line
