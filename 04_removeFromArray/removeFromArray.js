76e5cd23f15dae54e889ac1d904881b4c9470f7d
Justin Tan <tanjustin217@gmail.com>
2024-08-04 17:49:53 -0600

    Remove from array function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeFromArray) {
+  for (i = 0; i < removeFromArray.length; i++) {
+    let parameterItem = removeFromArray[i];
+    let indexOfParameterItem = array.indexOf(parameterItem);
+    while (indexOfParameterItem !== -1) {
+      array.splice(indexOfParameterItem, 1);
+      indexOfParameterItem = array.indexOf(parameterItem);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
