a9ffdcea5667a97f0e4b394331fc03d909e21662
Justin Tan <tanjustin217@gmail.com>
2024-08-04 16:26:35 -0600

    Implemented reverse string method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splitString = string.split("");
+  const reversedChars = splitString.reverse();
+  let reversedString = "";
+  for (i = 0; i < reversedChars.length; i++) {
+    reversedString += reversedChars[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
