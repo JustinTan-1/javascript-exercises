0d301ba3d81e652100b801c24a0b1153f0943965
Justin Tan <tanjustin217@gmail.com>
2024-08-04 18:25:59 -0600

    Implemented summation function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
+  let sum;
+  if (num1 > num2) {
+    sum = num2;
+    for (i = num2 + 1; i <= num1; i++) {
+      sum += i;
+    }
+  }
+  if (num1 < num2) {
+    sum = num1;
+    for (i = num1 + 1; i <= num2; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
