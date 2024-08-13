2d0aea980f5de69cb017c1eaf153b92b11466fd5
Justin Tan <tanjustin217@gmail.com>
2024-08-13 13:03:26 -0600

    Complete last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const date = new Date();
+  array.map((object) => {
+    if (object.yearOfDeath == undefined) {
+      object.yearOfDeath = date.getFullYear();
+    }
+  });
+  array.sort((a, b) => {
+    if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return array[0];
 };
 
 // Do not edit below this line
